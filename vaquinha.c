98380581c2180d479b8f78f755d74d5d4a9ba525
richardkayovidaldarosa <richard.vidal.rosa@escola.pr.gov.br>
2025-09-29 14:12:42 +0000

    SALVO

---
+++ vaquinha.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//Váriavel global
+float totalArrecadado = 0;
+
+//Procedimento: adiciona a contribuição de um participante
+void adicionaContribuicao(float valor){
+     totalArrecadado += valor;
+     printf("Contribuição de RS %.2f adicionada! Total:, R$",valor, totalArrecadado)
+}
+
+//Função: calcula quantidade de carne por pessoa(0,5kg por pessoa)
+
+float calcularCarne(int qtdPessoas){
+    return qtdPessoas * 0,5;
+}
+
+
+// Procedimento para mostrar o resumo
+void mostraResumo(int qtdPessoas){
+    float carne = calcularCarne(qtdPessoas);
+    float media = totalArrecadado / qtdPessoas
+    printf("\n=== RESUMO CHURRASCO ====\n");
+    printf("Participantes: %d\n", qtdPessoas);
+    printf("C")
+}
+
+int main(){                      
+ 
+  int pessoas;
+  float valor;
+
+  printf("Digite o número de participantes: ");
+  scanf("%d",&pessoas);
+
+  mostraResumo(pessoas);
+
+
+
+}
